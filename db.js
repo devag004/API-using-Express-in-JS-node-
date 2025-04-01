409ea43667c0d0f2251272b00e023a6b24b976cb
devag004 <da4656@srmist.edu.in>
2025-04-01 18:23:32 +0530

    Delete db.js

--- db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://0.0.0.0:27017/seca', {})
-.then((res)=> console.log("connected to db"))
-.catch((err)=> console.log("Error occoured " + err));
